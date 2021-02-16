e7cdba3f2cc229e199f0e8a574a129f2b302ffff
Rodrigo <55567123+rodrigodasilv@users.noreply.github.com>
2021-02-16 14:06:35 -0300

    Delete demanda1.py

--- demanda1.py
+++
@@ -1,7 +0,0 @@
-# Faça um programa que solicite o nome do usuário e posteriormente mostre uma
-# mensagem: Olá nomedousuário.
-
-usuario=''
-while not usuario.strip():
-    usuario = input('Informe seu nome: ')
-print ('Olá, ', usuario)
